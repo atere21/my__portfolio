05c39f47e3f4ae5e553b4765d00e27fe5528b35a
atere21 <tosinateret@gmail.com>
2023-01-26 12:16:37 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import './App.scss'
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className='App'>
-    hi
+    <div>
+      <Navbar/>
+      <Home/>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path='/' element= {<App/>} />
-    </Routes>
-  </Router>
 
-  
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
