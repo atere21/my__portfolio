2e00fd6f488d6d6dd6ac6e10df6c3b55d38d2e3c
atere21 <tosinateret@gmail.com>
2023-02-05 06:50:59 -0600

    task

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a132f] flex jusify-center items-center p-4'  >
-     <form method='post' action='https://getform.io/f/7db35b52-2504-4784-b131-ed5673e83177' className='flex flex-col max-w-[400px] pb-5 shadow-lg shadow-[#040c16] group container rounded-md jusify-center  mx-auto contactForm' >
-      <div className='pb-2' >
-        <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Contact</p>
+     <form method='post' action='https://getform.io/f/7db35b52-2504-4784-b131-ed5673e83177' className='flex flex-col max-w-[400px] pb-5 shadow-lg shadow-[#040c16] group container rounded-md jusify-center my-6  mx-auto contactForm' >
+      <div className='pb-2'>
+        <p className='text-4xl font-bold inline border-b-4  border-pink-600 text-gray-300 '>Contact</p>
         <p className='text-gray-300 py-3'>Submit the form below or shoot me an email-tosinateret@gmail.com</p>
       </div>
       <input className='bg-[#ccd6f6] p-2 ' type='text' placeholder='Name' name='name' />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import { FaBars, FaTimes,
      FaGithub,
      FaLinkedin,
+     FaTwitter,
     } 
      from "react-icons/fa";
 
@@ -81,23 +82,23 @@ const Navbar= () =>{
         </Link></li>
       </ul>
       {/* social-icons */}
-      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+      <div className=" lg:flex fixed flex-col top-[35%] left-0">
         <ul>
         <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
                     <a className="flex justify-between items-center w-full text-gray-300"
-                     href="/">
+                     href="https://www.linkedin.com/in/oluwatosin-atere-829a58205/">
                 LinkedIn <FaLinkedin size={30}/>
             </a>
         </li>
         <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
                     <a className="flex justify-between items-center w-full text-gray-300"
-                     href="/">
+                     href="https://github.com/atere21">
                 Github <FaGithub size={30}/>
             </a>
         </li>
         <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
                     <a className="flex justify-between items-center w-full text-gray-300"
-                     href="/">
+                     href="mailto:tosinateret@gmail.com">
                 Email <HiOutlineMail size={30}/>
             </a>
         </li>
@@ -107,6 +108,13 @@ const Navbar= () =>{
                 Resume <BsFillPersonLinesFill size={30}/>
             </a>
         </li>
+
+        <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+                    <a className="flex justify-between items-center w-full text-blue-300"
+                     href="https://twitter.com/flourishgold212">
+                Twitter <FaTwitter size={30}/>
+            </a>
+            </li>
         </ul>
       </div>
     </div>
