60fe62cd91c85d72c86954159de697b02d18a468
atere21 <you@tosinateret@gmail.com>
2023-09-26 09:40:15 -0700

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-full bg-[#0a132f] flex justify-center items-center mt-0 md:pt-[22rem] mb-8  '>
+    <div name='contact' className='w-full h-full bg-[#0a132f] flex justify-center items-center md:pt-[22rem] mt-[-10rem] '>
    <form
         method='post'
         action='https://getform.io/f/7db35b52-2504-4784-b131-ed5673e83177'
@@ -14,7 +14,7 @@ const Contact = () => {
         </div>
         <input className='bg-[#ccd6f6] p-2 rounded-md my-2 mx-4' type='text' placeholder='Name' name='name' />
         <input className='p-2 bg-[#ccd6f6] rounded-md my-2 mx-4' type='text' placeholder='Email' name='email' />
-        <textarea className='bg-[#ccd6f6] h-64 p-2 rounded-md my-2 mx-4 ' placeholder='Message' name='message' cols='30' rows='10'></textarea>
+        <textarea className='bg-[#ccd6f6] h-64 p-2 rounded-md my-2 mx-4 resize-none ' placeholder='Message' name='message' cols='30' rows='10'></textarea>
 
         <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 mx-4 py-3 my-3 w-[200px] sm:w-[289px] flex items-center justify-center rounded'>
           Let's Collaborate
